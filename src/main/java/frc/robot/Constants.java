38aa44206e6b2f875f2bf26a330ae504462ff268
aryan <52215513+importly@users.noreply.github.com>
2025-01-09 19:16:37 -0500

    initial

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,71 @@
+// Copyright 2021-2025 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotBase;
+
+/**
+ * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
+ * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics sim) and "replay"
+ * (log replay from a file).
+ */
+public final class Constants {
+  public static final Mode simMode = Mode.SIM;
+  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+  // Deadband
+  public static final double joystick_headband = 0.15;
+  // Robot dimensions and speed
+  public static final double MAX_LINEAR_SPEED = Units.feetToMeters(11.339);
+  public static final double TRACK_WIDTH_X =
+      Units.inchesToMeters(22.398); // X is perpendicular to the front of the robot
+  public static final double TRACK_WIDTH_Y =
+      Units.inchesToMeters(22.398); // Y is parallel to the front of the robot
+  public static final double DRIVE_GEAR_RATIO = 60.0 / 9.0;
+  public static final double TURN_GEAR_RATIO = 396.0 / 35.0; // (24.0 / 8) * (72.0 / 14);
+  // Motor and encoder constants
+  public static final int DRIVE_SPARK_MAX_FL = 24;
+  public static final int TURN_SPARK_MAX_FL = 25;
+  public static final int CANCODER_FL = 33;
+  public static final double ABSOLUTE_ENCODER_OFFSET_FL = 0.668;
+  public static final int DRIVE_SPARK_MAX_FR = 26;
+  public static final int TURN_SPARK_MAX_FR = 27;
+  public static final int CANCODER_FR = 30;
+  public static final double ABSOLUTE_ENCODER_OFFSET_FR = 0.780 - 0.5;
+  public static final int DRIVE_SPARK_MAX_BL = 23;
+  public static final int TURN_SPARK_MAX_BL = 22;
+  public static final int CANCODER_BL = 32;
+  public static final double ABSOLUTE_ENCODER_OFFSET_BL = 0.054;
+  public static final int DRIVE_SPARK_MAX_BR = 21;
+  public static final int TURN_SPARK_MAX_BR = 20;
+  public static final int CANCODER_BR = 31;
+  public static final double ABSOLUTE_ENCODER_OFFSET_BR = 0.646 + 0.5;
+  // Gyro
+  public static final int PIGEON2_CAN_ID = 26;
+  // Odometry
+  public static final double ODOMETRY_FREQUENCY = 250.0; // ! was oficially 100.0
+  public static final double WHEEL_RADIUS = Units.inchesToMeters(2.05) * 1 / 1.1132075472;
+  public static final boolean FIELD_MIRROR = false;
+
+  public static enum Mode {
+    /** Running on a real robot. */
+    REAL,
+
+    /** Running a physics simulator. */
+    SIM,
+
+    /** Replaying from a log file. */
+    REPLAY
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -0,0 +1,113 @@
+// Copyright 2021-2025 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot.subsystems.drive;
+
+import com.pathplanner.lib.config.ModuleConfig;
+import com.pathplanner.lib.config.RobotConfig;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import frc.robot.Constants;
+
+public class DriveConstants {
+  public static final double maxSpeedMetersPerSec = Constants.MAX_LINEAR_SPEED;
+  public static final double odometryFrequency = Constants.ODOMETRY_FREQUENCY; // Hz
+  public static final double trackWidth = Constants.TRACK_WIDTH_X;
+  public static final double wheelBase = Constants.TRACK_WIDTH_Y;
+  public static final double driveBaseRadius = Math.hypot(trackWidth / 2.0, wheelBase / 2.0);
+  public static final Translation2d[] moduleTranslations =
+      new Translation2d[] {
+        new Translation2d(trackWidth / 2.0, wheelBase / 2.0),
+        new Translation2d(trackWidth / 2.0, -wheelBase / 2.0),
+        new Translation2d(-trackWidth / 2.0, wheelBase / 2.0),
+        new Translation2d(-trackWidth / 2.0, -wheelBase / 2.0)
+      };
+
+  // Zeroed rotation values for each module, see setup instructions
+  public static final Rotation2d frontLeftZeroRotation = new Rotation2d(0.0);
+  public static final Rotation2d frontRightZeroRotation = new Rotation2d(0.0);
+  public static final Rotation2d backLeftZeroRotation = new Rotation2d(0.0);
+  public static final Rotation2d backRightZeroRotation = new Rotation2d(0.0);
+
+  // Device CAN IDs
+  public static final int pigeonCanId = Constants.PIGEON2_CAN_ID;
+
+  public static final int frontLeftDriveCanId = Constants.DRIVE_SPARK_MAX_FL;
+  public static final int backLeftDriveCanId = Constants.DRIVE_SPARK_MAX_BL;
+  public static final int frontRightDriveCanId = Constants.DRIVE_SPARK_MAX_FR;
+  public static final int backRightDriveCanId = Constants.DRIVE_SPARK_MAX_BR;
+
+  public static final int frontLeftTurnCanId = Constants.TURN_SPARK_MAX_FL;
+  public static final int backLeftTurnCanId = Constants.TURN_SPARK_MAX_BL;
+  public static final int frontRightTurnCanId = Constants.TURN_SPARK_MAX_FR;
+  public static final int backRightTurnCanId = Constants.TURN_SPARK_MAX_BR;
+
+  // Drive motor configuration
+  public static final int driveMotorCurrentLimit = 50;
+  public static final double wheelRadiusMeters = Constants.WHEEL_RADIUS;
+  public static final double driveMotorReduction = 60.0 / 9.0;
+  public static final DCMotor driveGearbox = DCMotor.getNeo550(1);
+
+  // Drive encoder configuration
+  public static final double driveEncoderPositionFactor =
+      2 * Math.PI / driveMotorReduction; // Rotor Rotations -> Wheel Radians
+  public static final double driveEncoderVelocityFactor =
+      (2 * Math.PI) / 60.0 / driveMotorReduction; // Rotor RPM -> Wheel Rad/Sec
+
+  // Drive PID configuration
+  public static final double driveKp = 0.0;
+  public static final double driveKd = 0.0;
+  public static final double driveKs = 0.0;
+  public static final double driveKv = 0.1;
+  public static final double driveSimP = 0.05;
+  public static final double driveSimD = 0.0;
+  public static final double driveSimKs = 0.0;
+  public static final double driveSimKv = 0.0789;
+
+  // Turn motor configuration
+  public static final boolean turnInverted = false;
+  public static final int turnMotorCurrentLimit = 20;
+  public static final double turnMotorReduction = 396.0 / 35.0;
+  public static final DCMotor turnGearbox = DCMotor.getNeo550(1);
+
+  // Turn encoder configuration
+  public static final boolean turnEncoderInverted = true;
+  public static final double turnEncoderPositionFactor = 2 * Math.PI; // Rotations -> Radians
+  public static final double turnEncoderVelocityFactor = (2 * Math.PI) / 60.0; // RPM -> Rad/Sec
+
+  // Turn PID configuration
+  public static final double turnKp = 4.54;
+  public static final double turnKd = 0.0;
+  public static final double turnSimP = 8.0;
+  public static final double turnSimD = 0.0;
+  public static final double turnPIDMinInput = 0; // Radians
+  public static final double turnPIDMaxInput = 2 * Math.PI; // Radians
+
+  // PathPlanner configuration
+  public static final double robotMassKg = 140.0;
+  public static final double robotMOI = 6.883;
+  public static final double wheelCOF = 1.2;
+  public static final RobotConfig ppConfig =
+      new RobotConfig(
+          robotMassKg,
+          robotMOI,
+          new ModuleConfig(
+              wheelRadiusMeters,
+              maxSpeedMetersPerSec,
+              wheelCOF,
+              driveGearbox.withReduction(driveMotorReduction),
+              driveMotorCurrentLimit,
+              1),
+          moduleTranslations);
+}
